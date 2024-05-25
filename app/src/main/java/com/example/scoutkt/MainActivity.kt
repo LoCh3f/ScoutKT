662a52b29e2758041c19ef7b7e8ceb5e7f5d2497
Aldo Visconti <l.c.aldovisconti@gmail.com>
2024-05-25 21:27:43 +0200

    New file Nav.kt, for navigation

--- app/src/main/java/com/example/scoutkt/MainActivity.kt
+++ app/src/main/java/com/example/scoutkt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.scoutkt.navigation.ComposeNavigation
 import com.example.scoutkt.ui.components.ScaffoldLayout
 import com.example.scoutkt.ui.components.StockCard
 import com.example.scoutkt.ui.theme.ScoutKTTheme
@@ -13,9 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ScoutKTTheme {
-                ScaffoldLayout("home",R.drawable.baseline_home_24)
-            }
+         ComposeNavigation()
         }
     }
 }
---
+++ app/src/main/java/com/example/scoutkt/navigation/Nav.kt
@@ -0,0 +1,31 @@
+package com.example.scoutkt.navigation
+
+import androidx.compose.runtime.Composable
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.scoutkt.R
+import com.example.scoutkt.ui.components.ScaffoldLayout
+
+
+@Composable
+fun ComposeNavigation() {
+    val  navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Home" ) {
+        composable("Home") {
+            ScaffoldLayout(activityName = "Home", id = R.drawable.baseline_home_24)
+        }
+        composable("Favorites") {
+            ScaffoldLayout(activityName = "Favorites", id = R.drawable.baseline_assistant_24)
+        }
+        composable("Alarms") {
+            ScaffoldLayout(activityName = "Alarms", id = R.drawable.baseline_alarm_24)
+        }
+        composable("Settings"){
+            ScaffoldLayout(activityName = "Settings", id =R.drawable.baseline_app_settings_alt_24 )
+
+        }
+
+    }
+}
