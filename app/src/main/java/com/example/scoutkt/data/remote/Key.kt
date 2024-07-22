d3997fc56e8fbffdeb6420107dc0a12d09b7b940
Aldo Visconti <aldo.visconti@studio.unibo.it>
2024-07-22 16:07:11 +0200

    Updated api key, since now new api are setted, download and apply(where removed) your own API key to use properly the app

--- app/src/main/java/com/example/scoutkt/data/remote/Key.kt
+++ app/src/main/java/com/example/scoutkt/data/remote/Key.kt
@@ -1,4 +1,4 @@
 package com.example.scoutkt.data.remote
 
-val key = "612406ed-df6a-407b-82f3-b831b411c14f"
-val coilApikey = "362688D5-F545-493A-AF15-D91EECF17B71"
+val key = ""
+val coilApikey = ""
